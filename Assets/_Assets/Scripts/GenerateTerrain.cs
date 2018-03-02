40ceae406e435b9ee22f4535736cfd3b5ecf8b00
medioman22 <matteo.macchini@gmail.com>
2018-03-02 18:44:44 +0100

    metadata

--- Assets/_Assets/Scripts/GenerateTerrain.cs
+++ Assets/_Assets/Scripts/GenerateTerrain.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class GenerateTerrain : MonoBehaviour {
 
-    int heightScale = 5;
-    float detailScale = 5.0f;
-
     public Renderer rend;
     public Vector3 pos;
 
@@ -17,15 +14,10 @@ public class GenerateTerrain : MonoBehaviour {
 
         pos = transform.position;
 
-        //Debug.Log(pos.x / 10);
-        //Debug.Log(pos.z / 10);
         if (Mathf.Abs((pos.x / 10 + pos.z / 10) % 2)==1)
         {
             rend.material.color = new Color(0, 0, 0);
         }
-
-
-        this.gameObject.AddComponent<MeshCollider>();
 
 	}
 	
